de22bf20bebdb786b74674e240a6b01c9aba88cf
Aredhel13 <Dkhakuzheva@mail.ru>
2019-10-16 21:33:50 +0300

    refresh

---
+++ src/main/java/com/Dinara/lab/task1/task4/Child.java
@@ -0,0 +1,8 @@
+package com.Dinara.lab.task1.task4;
+
+public class Child extends Parent {
+    public void print(){
+        System.out.println(a);
+    }
+    }
+
---
+++ src/main/java/com/Dinara/lab/task1/task4/Parent.java
@@ -0,0 +1,13 @@
+package com.Dinara.lab.task1.task4;
+
+public abstract class Parent {
+    int a=1;
+    public void print(){
+        System.out.println(a);
+    }
+    public static void main(String[] args) {new Child().print();}
+}
+
+//Опишем факультет: студенты и преподаватели, группы, расписание, предметы, их свойства.
+// Составить объектную модель факультета.
+// И написать тест(Генерация данных в тесте). Отчет для конкретного студента: расписание предметов, преподавателей
