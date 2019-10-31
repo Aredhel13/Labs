d588f5d8b74d52682a48c2420465fafdefd050f7
Aredhel13 <Dkhakuzheva@mail.ru>
2019-10-31 10:11:26 +0300

    refresh

--- src/test/java/com/Dinara/ind/labs/task/task2/task2.java
+++ src/test/java/com/Dinara/ind/labs/task/task2/task2.java
@@ -2,11 +2,11 @@ package com.Dinara.ind.labs.task.task2;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class task2 {
     @Test
-    public void Count(){
-       //    String text_count = "asd asd the why";
-       //   Assertions.assertEquals(2,text_count.Count.countChar["asd"]);
+    public void CountTest(){
+          Assertions.assertEquals(1,Count.count("i"));
     }
 }
